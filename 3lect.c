875e5354a8e811740f9cfff2eb2f6f1cb173f09b
dhruvrangholiya114 <130462930+dhruvrangholiya114@users.noreply.github.com>
2023-04-11 17:20:48 +0530

    Add files via upload

---
+++ 3lect.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+main()
+
+{
+	int a=15,b=5,c;
+	c=a+b;
+	printf("The addition of %d and %d is %d\n",a,b,c);
+	
+	c=a-b;
+	printf("The subtraction of %d and %d is %d\n",a,b,c);
+	
+	c=a*b;
+	printf("The multiplication of %d and %d is %d\n",a,b,c);
+	
+	c=a/b;
+	printf("The division of %d and %d is %d\n",a,b,c);
+	
+	c=a%b;
+	printf("The remainder of %d and %d is %d\n",a,b,c);
+	
+	
+}
